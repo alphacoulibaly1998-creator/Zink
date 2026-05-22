f8c70fb21326b8cfd9fa0c5d6774b296eaaf5e07
alphacoulibaly1998-creator <alphacoulibaly1998@gmail.com>
2026-05-22 11:08:12 +0000

    premier commit zink

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfCbAYK4QvYGAVUdujy7J46zQHnf00Teo",
+  authDomain: "zink-19923.firebaseapp.com",
+  projectId: "zink-19923",
+  storageBucket: "zink-19923.firebasestorage.app",
+  messagingSenderId: "922135364248",
+  appId: "1:922135364248:web:c4606bfd576c5dcc53c6ed"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
